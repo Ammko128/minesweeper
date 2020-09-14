beaff06044cae4bbbd428d996bcf440473befcfc
Ammko128 <ammar.veljagic@gmail.com>
2020-09-14 23:42:15 +0200

    Updates

---
+++ precache-manifest.b3090c070179979ccb29a15795bc8732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2168ff46fb02fc0d83d60e68a10af273",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "34830553ce6bf97c4309",
+    "url": "/minesweeper/static/css/main.1544835d.chunk.css"
+  },
+  {
+    "revision": "18e99b61a27997bba6da",
+    "url": "/minesweeper/static/js/2.5a210b5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.5a210b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34830553ce6bf97c4309",
+    "url": "/minesweeper/static/js/main.724afb0f.chunk.js"
+  },
+  {
+    "revision": "1e6bde3f490b690350a7",
+    "url": "/minesweeper/static/js/runtime-main.afd13353.js"
+  }
+]);
